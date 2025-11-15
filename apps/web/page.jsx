e9fb63f11752e423b199f4dc58216e82e99b3074
tothemoon07 <bcoronado000@gmail.com>
2025-11-15 17:41:06 -0400

    Add HomePage component with construction message

---
+++ apps/web/page.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div style={{ padding: "20px", textAlign: "center" }}>
+      <h1>Rifa en construcción</h1>
+      <p>Pronto podrás ver la rifa activa aquí.</p>
+    </div>
+  );
+}
