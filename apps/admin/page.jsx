1c7f16534966a4077c75253e1736cb53b546bec8
tothemoon07 <bcoronado000@gmail.com>
2025-11-16 01:58:49 -0400

    Delete apps/admin/page.jsx

--- apps/admin/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function AdminHome() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>Bienvenido al Panel</h1>
-      <p>Selecciona una sección del menú.</p>
-    </div>
-  );
-}
